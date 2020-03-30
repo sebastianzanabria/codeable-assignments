f909375486777cd975a77a0261795e9347b92451
sebas_z <zanabria.s@pucp.pe>
2020-03-30 17:53:49 -0500

    correction

--- module-1/week-3/day-1/readability.c
+++ module-1/week-3/day-1/readability.c
@@ -4,26 +4,26 @@
 #include <string.h>
 #include <ctype.h>
 
-int main(void){
-
+int main(void)
+{
     int index, W = 1, L = 0, S = 0;
     float Lpercent, Spercent;
 
     string text = get_string("Text: ");
-    int length =  strlen(text);
+    int length =  strlen(text); //length of text
 
     for (int i = 0; i < length; i++)
     {
         L += (bool) isalpha(text[i]);
-        W += (bool) isspace(text[i]) && !(bool) isspace(text[i+1]);
+        W += (bool) isspace(text[i]) && !(bool) isspace(text[i + 1]);// in case double space 
         S += text[i] == '.' || text[i] == '!' || text[i] == '?' ;
     }
 
-    Lpercent = L * 100 / (float) W;
+    Lpercent = L * 100 / (float) W; //letters percent
 
-    Spercent = S *100 / (float) W;
+    Spercent = S * 100 / (float) W; //sentences percent
 
-    index = round(0.0588 * Lpercent - 0.296 * Spercent - 15.8);
+    index = round(0.0588 * Lpercent - 0.296 * Spercent - 15.8); // Coleman-Liau index
 
     if (index < 1)
     {
@@ -38,5 +38,4 @@ int main(void){
     {
         printf("Grade %i\n", index);
     }
-
 }
